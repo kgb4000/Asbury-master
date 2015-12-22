cd452284b1d0425738a224a18e464ee68d703e77
kgb4000 <kester4000@gmail.com>
2015-12-22 18:07:36 -0500

    Initial commit. Already did static pages and built pages controller with some css. Used pg gem for postgresql

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+
+	def index
+	end
+
+	def pastor
+	end
+
+	def schedules
+	end
+
+	def staff
+	end
+
+	def history
+	end
+
+	def ministries
+	end
+
+end
